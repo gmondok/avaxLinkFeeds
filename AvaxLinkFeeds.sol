7b2d0da193c22e6c0a1626415a8a095bedf6f458
Gage Mondok <72169327+gmondok@users.noreply.github.com>
2021-09-14 12:52:31 -0400

    Update AvaxLinkFeeds.sol

--- AvaxLinkFeeds.sol
+++ AvaxLinkFeeds.sol
@@ -15,7 +15,6 @@ contract AvaxLinkFeeds {
      */
     constructor() {
         priceFeed = AggregatorV3Interface(0x5498BB86BC934c8D34FDA08E81D444153d0D06aD);
-        lastTime = block.timestamp;
     }
 
     /**
